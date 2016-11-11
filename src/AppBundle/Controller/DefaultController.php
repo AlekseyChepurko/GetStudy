1eb27879c055b83dd163a8d65edc92cbe3787448
AlekseyChepurko <aleksey.chepurko@gmail.com>
2016-11-11 23:28:18 +0300

    now its abstract class for all page controllers. implements getAuthError function;

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,29 +8,20 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-       
-        $category = $em
-            ->getRepository('AppBundle:Category')
-            ->findOneBy(
-                array(
-                    'name' => 'Main Posts',
-                ));
 
-        $posts = $category->getPosts()->getValues();
-        $posts = array_reverse($posts);
+    //private $error;
+    private $request;
+    private $session;
 
-        // Geting auth error
-        $session = $request->getSession();
+
+    public function getAuthError(Request $request){
+         // Geting auth error
+        $this->session = $request->getSession();
 
         $authErrorKey = Security::AUTHENTICATION_ERROR;
         $lastUsernameKey = Security::LAST_USERNAME;
@@ -38,9 +29,9 @@ class DefaultController extends Controller
         // get the error if any (works with forward and redirect -- see below)
         if ($request->attributes->has($authErrorKey)) {
             $error = $request->attributes->get($authErrorKey);
-        } elseif (null !== $session && $session->has($authErrorKey)) {
-            $error = $session->get($authErrorKey);
-            $session->remove($authErrorKey);
+        } elseif (null !== $this->session && $this->session->has($authErrorKey)) {
+            $error = $this->session->get($authErrorKey);
+            $this->session->remove($authErrorKey);
         } else {
             $error = null;
         }
@@ -50,14 +41,11 @@ class DefaultController extends Controller
         }
 
         // last username entered by the user
-        $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
+        $lastUsername = (null === $this->session) ? '' : $this->session->get($lastUsernameKey);
         $csrfToken = $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue();
 
-        // var_dump($error);
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-            'posts' => $posts,
-            'error' => $error,
-        ]);
+        return $error;
     }
+
+
 }
