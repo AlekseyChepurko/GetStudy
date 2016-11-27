988436288969c0a93f6736046daef0da2ffcb72e
AlekseyChepurko <aleksey.chepurko@gmail.com>
2016-11-28 00:54:34 +0300

    task selection added

--- src/AppBundle/Controller/SubjectPageController.php
+++ src/AppBundle/Controller/SubjectPageController.php
@@ -9,22 +9,58 @@ class SubjectPageController extends DefaultController{
 
 
     /**
-     * @Route("/subjects", name="subjects_page")
+     * @Route("/{examLevel}/{subject}")
      */
-	public function showSubjectsAction(Request $request){
+	public function showSubjectAction($examLevel, $subject, Request $request){
 
 		$em = $this->getDoctrine()->getManager();
 
-		$subjects = $em
-					->getRepository('AppBundle:Subject')
-					->findAll()
-					;	
+		$authError = $this->getAuthError($request);
+
+		$queryBuilder = $em->createQueryBuilder();
+
+		$tasks = $queryBuilder->select('task')
+								->from('AppBundle:Task', 'task')
+								->innerJoin('task.taskType', 'taskType')
+								->innerJoin('taskType.subject', 'subject')
+								->where("subject.name=:subject")
+								->andWhere("subject.useState=:examLevel")
+								->setParameter('subject', $subject)
+								->setParameter('examLevel', $examLevel)
+								->getQuery()
+								->getResult();
+
+
+		// var_dump($tasks);
+		return $this->render('default/subjects.html.twig', array(
+			'error' => $authError,
+			'tasks' => $tasks
+			));
+	}
+
+	/**
+     * @Route("/subjects", name="subjects_page")
+     */
+	public function showLevelChoiceAction(Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
 
 		$authError = $this->getAuthError($request);
 
-		return $this->render('default/subjects.html.twig', array(
-			'subjects' => $subjects,
+		$useSubjects = $em
+						->getRepository('AppBundle:Subject')
+						->findByUseState("егэ")
+						;
+
+		$bseSubjects =  $em
+						->getRepository('AppBundle:Subject')
+						->findByUseState("гиа")
+						;
+
+		return $this->render('default/choiceSubject.html.twig', array(
 			'error' => $authError,
+			'useSubjects' => $useSubjects,
+			'bseSubjects' => $bseSubjects
 			));
 	}
 
